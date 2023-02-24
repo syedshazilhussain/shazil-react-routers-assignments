9ab39a0703c39e07d206f353ff0628df241dda98
syedshazilhussain <119860685+syedshazilhussain@users.noreply.github.com>
2023-02-24 16:01:01 +0500

    shazil react routers assignments
    shazil react routers assignments

---
+++ App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Router from './routers/routers';
+// import { Routes } from 'react-router';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Router/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
